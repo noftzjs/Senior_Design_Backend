d9fdfeb7ace442cfa8a83a07feab53744b18ae49
Jerry Noftz <noftzjs@mail.uc.edu>
2020-10-29 16:57:47 -0400

    Updating the project

--- route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-function eRoutes() {
-    const router = express.Router();
-    var users = require('./repository/user/user.routes')(router);
-    /* var department = require('./repository/department/department.routes')(router);
-    return router; */
-}
-module.exports = eRoutes;
