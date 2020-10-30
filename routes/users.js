57d9b9e8d65cb548b16d33341348474280929b01
uc-irelanjc <59898804+uc-irelanjc@users.noreply.github.com>
2020-10-30 14:31:00 -0400

    MyDatabaseCode

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'seniordesignserver.database.windows.net',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'admin20', //update me
+                password: 'Gr0up20$'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'SeniorDesignDatabase'  //update me
+        }
+    }; 
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.  
+        console.log("Connected");  
+        executeStatement();  
+    });  
+  
+    var Request = require('tedious').Request;  
+    var TYPES = require('tedious').TYPES;  
+  
+    function executeStatement() {  
+        request = new Request("SELECT * FROM Users", function(err) {  
+        if (err) {  
+            console.log(err);}  
+        });  
+        var result = []; 
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+              if (column.value === null) {
+                console.log('NULL');
+              } else {
+                result+= column.value + " ";
+              }
+            });
+            console.log(result);
+            result = [];
+        });  
+  
+        request.on('done', function(rowCount, more) {  
+        console.log(rowCount + ' rows returned');  
+        });  
+        connection.execSql(request);  
+    }
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(result);
 });
 
 module.exports = router;
